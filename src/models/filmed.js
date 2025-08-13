24b7f4b4191864aa27041c18d518b8549a488723
Huỳnh Tấn Mạnh <manhhuynh1709@gmail.com>
2025-08-13 20:17:14 +0700

    fix lỗi đặt tên model

--- src/models/filmed.js
+++ src/models/filmed.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const filmed = new Schema({
+const movies-watched = new Schema({
   userId: { type: ObjectId, ref: 'user' },
   slug: { type: String },
   watchedAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('filmed', filmed);
+module.exports = mongoose.model('filmed', movies-watched);
